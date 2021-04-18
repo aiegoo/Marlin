ce5a9e37dc585a7cb760f8a7a35d6908afa65bf4
aiegoo <eozz21@gmail.com>
2021-04-18 10:51:49 +0900

    fix more conflicts

--- Marlin/Marlin.ino
+++ Marlin/Marlin.ino
@@ -1,85 +1,41 @@
-<<<<<<< HEAD
 /*==============================================================================
 
   Marlin Firmware
 
   (c) 2011-2020 MarlinFirmware
-=======
-/*
-================================================================================
-
-  Marlin Firmware
-
-  (c) 2011-2018 MarlinFirmware
->>>>>>> 1314b31d97bba8cd74c6625c47176d4692f57790
   Portions of Marlin are (c) by their respective authors.
   All code complies with GPLv2 and/or GPLv3
 
 ================================================================================
 
-<<<<<<< HEAD
 Greetings! Thank you for choosing Marlin 2 as your 3D printer firmware.
 
 To configure Marlin you must edit Configuration.h and Configuration_adv.h
 located in the root 'Marlin' folder. Check our Configurations repository to
-=======
-Greetings! Thank you for choosing Marlin as your 3D printer firmware.
-
-To configure Marlin you must edit Configuration.h and Configuration_adv.h
-located in the root 'Marlin' folder. Check the example_configurations folder to
->>>>>>> 1314b31d97bba8cd74c6625c47176d4692f57790
 see if there's a more suitable starting-point for your specific hardware.
 
 Before diving in, we recommend the following essential links:
 
 Marlin Firmware Official Website
 
-<<<<<<< HEAD
   - https://marlinfw.org/
-=======
-  - http://marlinfw.org/
->>>>>>> 1314b31d97bba8cd74c6625c47176d4692f57790
     The official Marlin Firmware website contains the most up-to-date
     documentation. Contributions are always welcome!
 
 Configuration
 
-<<<<<<< HEAD
+
   - https://github.com/MarlinFirmware/Configurations
     Example configurations for several printer models.
 
-=======
->>>>>>> 1314b31d97bba8cd74c6625c47176d4692f57790
+
   - https://www.youtube.com/watch?v=3gwWVFtdg-4
     A good 20-minute overview of Marlin configuration by Tom Sanladerer.
-    (Applies to Marlin 1.0.x, so Jerk and Acceleration should be halved.)
-    Also... https://www.google.com/search?tbs=vid%3A1&q=configure+marlin
 
-<<<<<<< HEAD
   - https://marlinfw.org/docs/configuration/configuration.html
-=======
-  - http://marlinfw.org/docs/configuration/configuration.html
->>>>>>> 1314b31d97bba8cd74c6625c47176d4692f57790
     Marlin's configuration options are explained in more detail here.
 
-Getting Help
-
-<<<<<<< HEAD
-  - https://reprap.org/forum/list.php?415
-=======
-  - http://forums.reprap.org/list.php?415
->>>>>>> 1314b31d97bba8cd74c6625c47176d4692f57790
-    The Marlin Discussion Forum is a great place to get help from other Marlin
-    users who may have experienced similar issues to your own.
-
-  - https://github.com/MarlinFirmware/Marlin/issues
-    With a free GitHub account you can provide us with feedback, bug reports,
-    and feature requests via the Marlin Issue Queue.
-
 Contributing
-
-<<<<<<< HEAD
-  - https://marlinfw.org/docs/development/contributing.html
     If you'd like to contribute to Marlin, read this first!
 
   - https://marlinfw.org/docs/development/coding_standards.html
@@ -87,11 +43,10 @@ Contributing
 
 
 ------------------------------------------------------------------------------*/
-=======
+
   - http://marlinfw.org/docs/development/contributing.html
     If you'd like to contribute to Marlin, read this first!
 
   - http://marlinfw.org/docs/development/coding_standards.html
     Before submitting code get to know the Coding Standards.
 */
->>>>>>> 1314b31d97bba8cd74c6625c47176d4692f57790
